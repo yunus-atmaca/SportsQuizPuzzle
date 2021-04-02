e328badc0f7d219ac840fc9ae0707a901e373747
yunus-atmaca <yunusatmacabm@gmail.com>
2021-04-02 16:47:16 +0300

    add imp. main-act-ui

--- app/src/main/java/com/sportsquizpuzzle/MainActivity.java
+++ app/src/main/java/com/sportsquizpuzzle/MainActivity.java
@@ -3,12 +3,62 @@ package com.sportsquizpuzzle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.sportsquizpuzzle.utils.SystemUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "Main-Activity";
+
+    private ImageView play;
+    private ImageView levelList;
+    private ImageView settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SystemUtils.enableFullScreenUI(this);
         setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init() {
+        play = findViewById(R.id.play);
+        play.setOnClickListener(this);
+        levelList = findViewById(R.id.level_list);
+        levelList.setOnClickListener(this);
+        settings = findViewById(R.id.settings);
+        settings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.play){
+            onPlayClick();
+        }else if(view.getId() == R.id.settings){
+            onSettingsClick();
+        }else if(view.getId() == R.id.level_list){
+            onLevelListClick();
+        }else{
+            Log.d(TAG, "Unimplemented call");
+        }
+    }
+
+    private void onSettingsClick() {
+        Log.d(TAG, "onSettingsClick");
+    }
+
+    private void onLevelListClick() {
+        Log.d(TAG, "onLevelListClick");
+    }
+
+    private void onPlayClick() {
+        Log.d(TAG, "onPlayClick");
     }
 }
---
+++ app/src/main/java/com/sportsquizpuzzle/utils/SystemUtils.java
@@ -0,0 +1,26 @@
+package com.sportsquizpuzzle.utils;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SystemUtils {
+    public static void enableFullScreenUI(AppCompatActivity activity){
+        View decorView = activity.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    public static void disableFullScreenUI(AppCompatActivity activity){
+        View decorView = activity.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+}
